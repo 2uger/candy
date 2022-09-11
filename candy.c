677b9c6df057074b35ee70775f49fbca3967e5a6
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-09-11 13:51:52 +0300

    can move cursor on terminal

--- candy.c
+++ candy.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +10,7 @@
 #define CTRLKEY(k) ((k) & 0x1f)
 
 struct EditorConfig {
+    int cx, cy;
     int screen_rows;
     int screen_cols;
     struct termios orig_termios;
@@ -68,6 +70,31 @@ editor_read_key()
 }
 
 int
+get_cursor_position(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+    return 0;
+}
+
+int
 get_window_size(int *rows, int *cols)
 {
     struct winsize ws;
@@ -75,8 +102,7 @@ get_window_size(int *rows, int *cols)
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
             return -1;
-        editor_read_key();
-        return -1;
+        return get_cursor_position(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -85,7 +111,60 @@ get_window_size(int *rows, int *cols)
     
 }
 
+/*** Append buffer ***/
+
+#define ABUF_INIT {NULL, 0}
+
+// Append buffer
+struct abuf {
+    char *b;
+    int len;
+};
+
+void
+ab_append(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL)
+        return;
+    
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void
+ab_free(struct  abuf *ab)
+{
+    free(ab->b);
+}
+
 /*** input ***/
+
+#define MV_UP    'k'
+#define MV_DOWN  'j'
+#define MV_LEFT  'h'
+#define MV_RIGHT 'l'
+
+void
+editor_move_cursor(char key)
+{
+    switch (key) {
+        case MV_DOWN:
+            config.cy = (config.cy != config.screen_rows - 1 ? config.cy + 1 : config.cy);
+            break;
+        case MV_UP:
+            config.cy = (config.cy != 0 ? config.cy - 1 : config.cy);
+            break;
+        case MV_LEFT:
+            config.cx = (config.cx != 0 ? config.cx - 1 : config.cx);
+            break;
+        case MV_RIGHT:
+            config.cx = (config.cx != config.screen_cols - 1 ? config.cx + 1 : config.cx);
+            break;
+    }
+
+}
 
 void
 editor_process_keypress()
@@ -97,28 +176,48 @@ editor_process_keypress()
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        case MV_DOWN:
+        case MV_UP:
+        case MV_LEFT:
+        case MV_RIGHT:
+            editor_move_cursor(c);
+            break;
     }
 }
 
 /*** output ***/
 
 void
-editor_draw_rows()
+editor_draw_rows(struct abuf *ab)
 {
     for (int y = 0; y < config.screen_rows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        ab_append(ab, "~", 1);
+        ab_append(ab, "\x1b[K", 3);
+
+        if (y < config.screen_rows - 1) {
+            ab_append(ab, "\r\n", 2);
+        }
     }
 }
 
 void
 editor_refresh_screen()
 {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
 
-    editor_draw_rows();
+    ab_append(&ab, "\x1b[?25l", 6);
+    ab_append(&ab, "\x1b[H", 3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editor_draw_rows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", config.cy + 1, config.cx + 1);
+    ab_append(&ab, buf, strlen(buf));
+
+    ab_append(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    ab_free(&ab);
 }
 
 /*** init ***/
@@ -126,6 +225,9 @@ editor_refresh_screen()
 void
 init_editor()
 {
+    config.cx = 0;
+    config.cy = 0;
+
     if (get_window_size(&config.screen_rows, &config.screen_cols) == -1)
         die("get_window_size");
 }
