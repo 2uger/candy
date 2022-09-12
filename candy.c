3782809f85acc3cf0e07059c345130a724885068
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-09-12 11:44:36 +0300

    can read file for now, got segmentation fault

--- candy.c
+++ candy.c
@@ -9,10 +9,17 @@
 
 #define CTRLKEY(k) ((k) & 0x1f)
 
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
+
 struct EditorConfig {
     int cx, cy;
     int screen_rows;
     int screen_cols;
+    int numrows;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -46,6 +53,7 @@ enable_raw_mode()
 
     struct termios raw = config.orig_termios;
 
+    // man termios (for flag meanings)
     raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -191,8 +199,15 @@ void
 editor_draw_rows(struct abuf *ab)
 {
     for (int y = 0; y < config.screen_rows; y++) {
-        ab_append(ab, "~", 1);
-        ab_append(ab, "\x1b[K", 3);
+        if (y >= config.numrows) {
+            ab_append(ab, "~", 1);
+        } else {
+            int len = config.row[y].size;
+            if (len > config.screen_cols)
+                len = config.screen_cols;
+            ab_append(ab, config.row[y].chars, len);
+        }
+        ab_append(ab, "\x1b[K", 3);  // clear current line
 
         if (y < config.screen_rows - 1) {
             ab_append(ab, "\r\n", 2);
@@ -205,19 +220,58 @@ editor_refresh_screen()
 {
     struct abuf ab = ABUF_INIT;
 
-    ab_append(&ab, "\x1b[?25l", 6);
-    ab_append(&ab, "\x1b[H", 3);
+    ab_append(&ab, "\x1b[?25l", 6); // hide cursor
+    ab_append(&ab, "\x1b[H", 3);    // change position of cursor to 0,0
 
     editor_draw_rows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", config.cy + 1, config.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", config.cy + 1, config.cx + 1);  // move cursor to certain position
     ab_append(&ab, buf, strlen(buf));
 
-    ab_append(&ab, "\x1b[?25h", 6);
+    ab_append(&ab, "\x1b[?25h", 6);  // show cursor (to avoid flickering when redraw)
 
     write(STDOUT_FILENO, ab.b, ab.len);
     ab_free(&ab);
+}
+
+/*** row operations ***/
+
+void
+editor_append_row(char *s, size_t len)
+{
+    config.row = realloc(config.row, sizeof(erow) + (config.numrows + 1));
+
+    struct erow *r = &config.row[config.numrows];
+    r->size = len;
+    r->chars = malloc(len + 1);
+    memcpy(r->chars, s, len);
+
+    r->chars[len] = '\0';
+
+    config.numrows++;
+}
+
+/*** file i/o ***/
+
+void
+editor_open(char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+        die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+        editor_append_row(line, linelen);
+    }
+    free(line);
+    fclose(fp);
 }
 
 /*** init ***/
@@ -227,16 +281,21 @@ init_editor()
 {
     config.cx = 0;
     config.cy = 0;
+    config.numrows = 0;
+    config.row = NULL;
 
     if (get_window_size(&config.screen_rows, &config.screen_cols) == -1)
         die("get_window_size");
 }
 
 int
-main()
+main(int argc, char *argv[])
 {
     enable_raw_mode();
     init_editor();
+    if (argc >= 2)
+        editor_open(argv[1]);
+
     char c;
     while (1) {
         editor_refresh_screen();
